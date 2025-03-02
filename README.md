a1c2240eaf77ed1f1ade3b7cebc892441fbbcc14
dmaymay <dmayoracurzio@gmail.com>
2025-03-02 21:53:59 +0100

    x

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # airtable_rs
 Airtable API helper for Rust
+
+# TO DO
+
+- Get Records // also from specified view
+- Create Record function (if no rec*****) 
+- Update Record function (if contains rec****) PATCH or PUT option?
+- Infer types from table metadata / field types
+- Print out map with fields and inferred types
+- Test actually using data
+- Modify data -> back to update or create record ( x type to string?)
+- Merge record function ( batch into create or update according to record status) --> performUpsert merge on id??
+- Recursive offset when getting records -> specify MaxRecords if user wants to limit records
+- Manage Limit of 10 records when creating or updating records (Batch update always if possible)
+- Delete Records -> Manage 10 limit deleting
+
+- Manage Batch call / sec limits (specially with async)
+
+- Token and Base validation
+- Upload Attachments
+- Create / update fields
+- Create / update Tables
+- Sync CSV Data -> Create table from csv?
+
+- Handle linked records (automatically fetch data from linked table (recursion limit?????))
+
+- List Bases
+- Get Base Schema
+- Create Bases
+
+- Create Client (logged in with credential )
+- Error Handling
+
+
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,106 @@
 use dotenv::dotenv;
-use std::env;
+use reqwest::{self, header, Client};
+use serde::Serialize;
+use std::{collections::HashMap, env};
+use serde_json::{json, Value};
+use tokio::time::{sleep, Duration};
 
-fn main() {
+#[tokio::main]
+async fn main() {
     dotenv().ok();
-    let api_key = env::var("AIRTABLE_KEY");
-    let base_id = env::var("BASE_ID");
-    let table_1_id = env::var("TABLE_1_ID");
-    let filtered_view = env::var("FILTERED_VIEW_ID");
-    let unfiltered_view = env::var("UNFILTERED_VIEW_ID");
 
-    println!("Access Key {:?}", api_key);
-    println!("Base {:?}", base_id);
+    let api_key = env::var("AIRTABLE_KEY").expect("Missing AIRTABLE_KEY");
+    let base_id = env::var("BASE_ID").expect("Missing BASE_ID");
+    let table_1_id = env::var("TABLE_1_ID").expect("Missing TABLE_1_ID");
+/*     let filtered_view = env::var("FILTERED_VIEW_ID").expect("Missing FILTERED_VIEW_ID");
+    let unfiltered_view = env::var("UNFILTERED_VIEW_ID").expect("Missing UNFILTERED_VIEW_ID"); */
 
+    let mut fields = HashMap::new();
+    fields.insert("Name".to_string(), json!("New Task"));
+    //fields.insert("Status".to_string(), json!("[To Do]"));
+    fields.insert("Due".to_string(), json!("2025-03-01T12:00:00.000Z"));
+    fields.insert("integer".to_string(), json!(10));
+    fields.insert("float".to_string(), json!(3.1415));
+    fields.insert("positive_integer".to_string(), json!(7));
+    fields.insert("poor_text".to_string(), json!("This is some text."));
+    fields.insert("Rich text".to_string(), json!("Rich text example\n\n```\ncode snippet\n```"));
+
+    let record = json!({ "fields": fields });
+
+    match create_record(&base_id, &table_1_id, &api_key, &record).await {
+        Ok(_) => println!("Record created successfully!"),
+        Err(err) => println!("Error creating record: {}", err),
+    }
+    
+    sleep(Duration::from_secs(3)).await;
+
+    get_records(&base_id, &table_1_id, &api_key, &Some("recbztNx2W30I2eBA".to_string())).await;
+
+    
+    //get_records(&base_id, &table_1_id, &api_key, &None);
+}
+
+/* fn main() {
+    get_request();
+    sleep(Duration::from_secs(3)); // Sleep for 3 seconds
+    post_request();
+} */
+
+async fn get_records(
+    base_id: &String,
+    table_id: &String,
+    api_key: &String,
+    record_id: &Option<String>
+) -> Result<(), reqwest::Error> {
+    let url = match record_id {
+        Some(id) => format!("https://api.airtable.com/v0/{}/{}/{}", base_id, table_id, id),
+        None => format!("https://api.airtable.com/v0/{}/{}", base_id, table_id),
+    };
+
+    let client = Client::new();
+
+    let response = client
+        .get(&url)
+        .header(header::AUTHORIZATION, format!("Bearer {}", api_key))
+        .send()
+        .await?;
+
+    if response.status().is_success() {
+        let body = response.text().await?;
+        println!("Response: {}", body);
+    } else {
+        println!("Request failed with status: {}", response.status());
+    }
+
+    Ok(())
+}
+
+async fn create_record(
+    base_id: &str,
+    table_id: &str,
+    api_key: &str,
+    record: &Value,
+) -> Result<(), reqwest::Error> {
+    let url = format!("https://api.airtable.com/v0/{}/{}", base_id, table_id);
+    
+    let client = Client::new();
+
+    let response = client
+        .post(&url)
+        .header(header::AUTHORIZATION, format!("Bearer {}", api_key))
+        .header(header::CONTENT_TYPE, "application/json")
+        .json(record)
+        .send()
+        .await?;
+
+    if response.status().is_success() {
+        let body = response.text().await?;
+        println!("Record Created: {}", body);
+    } else {
+        println!("Request failed with status: {}", response.status());
+        let error_body = response.text().await?;
+        println!("Error response: {}", error_body);
+    }
+
+    Ok(())
 }
